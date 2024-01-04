3c5a2486e7aeae21e090030239888e71110cb68d
MOB <mob5832@gmail.com>
2024-01-04 15:50:17 +0700

    Initialize

---
+++ src/main/java/com/tech/camunda/SpringCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.tech.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCamundaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tech/camunda/SpringCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamundaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
